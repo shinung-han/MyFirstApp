934538cadd52b317a8afd7074ba81a70f4cb4b22
hanshinung <hanshinung@gmail.com>
2024-08-29 23:26:15 +0900

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by 한신웅 on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
